6ca9de27417db2fce18c4c67f16423711440db14
Ivan Kuznetsov <96410453+MightComeback@users.noreply.github.com>
2023-02-14 10:56:00 +0200

    Crypto project added, some tech

--- utils/data.ts
+++ utils/data.ts
@@ -36,6 +36,14 @@ export const Icons = [
     image: '/node-js.svg',
   },
   {
+    name: 'Express JS',
+    image: '/express-js.svg',
+  },
+  {
+    name: 'Vite',
+    image: '/vite.png',
+  },
+  {
     name: 'Redux',
     image: '/redux.svg',
   },
@@ -74,7 +82,6 @@ export const Icons = [
 ];
 
 export const ProjectsData = [
-  
   {
     title: "Online Clothing Store",
     description: "Fully operational clothing store website with Stripe.",
@@ -104,6 +111,16 @@ export const ProjectsData = [
     ],
     details: "Random funny tool to kill some time. Had this idea in mind, so why not implement.",
     github: "https://github.com/MightComeback/youtube-randomizer",
+  },
+  {
+    title: "React Crypto & Todo Panel",
+    description: "An app with Crypto Market Charts & Data and a To-do list.",
+    image: "/react-admin.png",
+    tech: [
+      "React", "Node JS", "Redux", "TypeScript", "Express JS", "MongoDb", "CoinMarketCap API"
+    ],
+    details: "Using CoinMarketCap API I created a simple app with Crypto Coin data and Charts. Back-end using Node JS, Express JS and MongoDb. Using Redux Toolkit for client-side state management.",
+    github: "https://github.com/MightComeback/react-panel",
   },
   {
     title: "Rain & Chill",
@@ -139,6 +156,16 @@ export const ProjectsData = [
 
 export const ProjectsDataUkrainian = [
   {
+    title: "Онлайн Магазин Одягу",
+    description: "Повністю функціональний магазин одягу на Stripe.",
+    image: "/online-store.png",
+    tech: [
+      "Stripe", "Axios", "Postgres", "Prisma", "NextJS 13", "React", "TypeScript", "TailwindCSS"
+    ],
+    details: "Це інтернет-магазин з продажу одягу. Я використовую Stripe для оформлення замовлень та Axios для отримання даних, зберігаю продукти в базі даних Postgres (локально), а також у Stripe. Використовую стек NextJS 13 із TypeScript і TailwindCSS для стилізації.",
+    github: "https://github.com/MightComeback/online-store",
+  },
+  {
     title: "Додаток Онлайн Чат",
     description: "Додаток онлайн-чату, що використовує Next 13 і Typescript, а також компоненти React разом із Google Auth, використовує MongoDb для зберігання користувачів і повідомлень, та TailwindCSS для стилізації.",
     image: "/chat-app.png",
@@ -157,6 +184,16 @@ export const ProjectsDataUkrainian = [
     ],
     details: "Смішний інструмент, щоб вбити час. Мав цю ідею в голові, то чому б не реалізувати.",
     github: "https://github.com/MightComeback/youtube-randomizer",
+  },
+  {
+    title: "Криптовалюта & To-do Панель",
+    description: "Додаток з інформацією про криптовалюти та чарти & To-do список",
+    image: "/react-admin.png",
+    tech: [
+      "React", "Node JS", "Redux", "TypeScript", "Express JS", "MongoDb", "CoinMarketCap API"
+    ],
+    details: "Використовуючи API CoinMarketCap, я реалізував простий додаток з даними Криптовалют і діаграмами. Бекенд із використанням Node JS, Express JS і MongoDb. Використання Redux Toolkit для керування State на стороні клієнта.",
+    github: "https://github.com/MightComeback/react-panel",
   },
   {
     title: "Безкінечний Дощ",
